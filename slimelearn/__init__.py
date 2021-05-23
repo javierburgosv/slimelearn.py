bdbfa8f3dc8ce97eab70736e53fdf50ac13a00a8
javierburgosv <javier.burgosv@alumnos.upm.es>
2021-05-23 17:07:48 +0200

    Packing SetUp

---
+++ slimelearn/__init__.py
@@ -0,0 +1,5 @@
+"""
+slimelearn.py version 0.8.0
+"""
+
+__version__ = "0.8.0"
--- slimelearn/slimelearn.py
+++ slimelearn/slimelearn.py
@@ -8,8 +8,6 @@ import json
 import nest_asyncio
 nest_asyncio.apply()
 
-__version__ = "0.8.0"
-
 class SlimeLearn:
 
     ws = None
